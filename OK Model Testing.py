829b8aff6bb5b3774bf7eb67bf9a27297b1026b0
patel12m <144816590+patel12m@users.noreply.github.com>
2023-11-26 16:25:21 -0500

    Add files via upload

---
+++ OK Model Testing.py
@@ -0,0 +1,44 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
+import matplotlib.pyplot as plt
+
+# Load the previously trained and saved model
+model = load_model('/content/drive/MyDrive/Project 2 Data/okmodel.h5')
+
+# Function to process and predict on a single image
+def process_and_predict_image(img_path, model):
+    # Load the image
+    img = image.load_img(img_path, target_size=(100, 100))
+    # Convert the image to a numpy array
+    img_array = image.img_to_array(img)
+    # Add a batch dimension and rescale
+    img_array = np.expand_dims(img_array, axis=0) / 255.0
+    # Make predictions
+    predictions = model.predict(img_array)
+    # Get the class with the highest probability
+    predicted_class = np.argmax(predictions, axis=1)
+    return predictions, predicted_class
+
+# Function to display the prediction
+def display_prediction(img_path, model, class_labels):
+    predictions, predicted_class = process_and_predict_image(img_path, model)
+    # Load the image for displaying
+    img = image.load_img(img_path)
+    plt.imshow(img)
+    plt.axis('off')  
+    # Show the prediction probabilities for each class
+    plt.title(f"Predicted: {class_labels[predicted_class[0]]}\n" +
+              "\n".join([f"{class_labels[i]}: {predictions[0][i]*100:.2f}%" for i in range(len(class_labels))]))
+    plt.show()
+
+# Define the path to your test image and class labels
+test_image_path1 = '/content/drive/MyDrive/Project 2 Data/Data/Test/Medium/Crack__20180419_06_19_09,915.bmp'
+class_labels1 = ['Large Crack', 'Medium Crack', 'Small Crack', 'No Crack']  
+
+test_image_path2 = '/content/drive/MyDrive/Project 2 Data/Data/Test/Large/Crack__20180419_13_29_14,846.bmp'
+class_labels2 = ['Large Crack', 'Medium Crack', 'Small Crack', 'No Crack']  
+
+# Display the model's prediction for the test image
+display_prediction(test_image_path1, model, class_labels1)
---
+++ Perfect Model DCNN.py
@@ -0,0 +1,87 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# Create an instance of ImageDataGenerator for training and validation data
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
+
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+# Point to the directory where training data is stored
+train_generator = train_datagen.flow_from_directory(
+    '/content/drive/MyDrive/Project 2 Data/Data/Train',
+    target_size=(100, 100),  # The target image size
+    batch_size=32,  # The batch size
+    class_mode='categorical'  # Assuming you are doing a multi-class classification
+)
+
+# Point to the directory where validation data is stored
+val_generator = val_datagen.flow_from_directory(
+    '/content/drive/MyDrive/Project 2 Data/Data/Validation',
+    target_size=(100, 100),
+    batch_size=32,
+    class_mode='categorical'
+)
+
+# Build the model architecture
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(100, 100, 3)),
+    MaxPooling2D(2, 2),
+    Conv2D(64, (3, 3), activation='relu', input_shape=(100, 100, 3)),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dropout(0.5),
+    Dense(train_generator.num_classes, activation='softmax')  # Set the output layer to match the number of classes
+])
+
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+history = model.fit(
+    train_generator,
+    validation_data=val_generator,
+    epochs=100,
+    steps_per_epoch=train_generator.samples // train_generator.batch_size,
+    validation_steps=val_generator.samples // val_generator.batch_size
+)
+
+# Save the model
+model_save_path = '/content/drive/MyDrive/Project 2 Data/Perfectmodel.h5'
+model.save(model_save_path)
+
+# Model Evaluation
+plt.figure(figsize=(12, 4))
+
+# Plot training & validation accuracy values
+plt.subplot(1, 2, 1)
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Validation'], loc='upper left')
+
+# Plot training & validation loss values
+plt.subplot(1, 2, 2)
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Validation'], loc='upper left')
+
+plt.tight_layout()
+plt.show()
+
